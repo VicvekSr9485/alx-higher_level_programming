1e078e9b1fc3ba2f491562ebda3460b6eb27807c
Oso Olamide <olamideoso65@gmail.com>
2022-05-31 12:45:46 -0700

    uppercase string

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for c in str:
+        c = ord(c)
+        if c >= 97 and i <= 122:
+            c -= 32
+        c = chr(c)
+        print("{}".format(c), end='')
+    print("")
