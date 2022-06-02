ea39d0c8e2ae74e120d215f87f3cc0b4a701b7ea
Oso Olamide <olamideoso65@gmail.com>
2022-06-02 10:14:57 -0700

    argument count

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-print("{} argument:".format(int(len(sys.argv))))
-print("1: {}".format(sys.argv[0]))
+
+def main():
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(argc))
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+
+if __name__ == "__main__":
+    main()
