bb8511497d8dd1ec6a1b74c756819df13852025f
VicvekSr <85700432+VicvekSr9485@users.noreply.github.com>
2022-06-02 20:45:03 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import sys
 
+
 def main():
     argc = len(sys.argv) - 1
     if argc == 0:
@@ -13,6 +14,7 @@ def main():
         print("{:d} arguments:".format(argc))
         for i in range(1, argc + 1):
             print("{}: {}".format(i, sys.argv[i]))
+
 
 if __name__ == "__main__":
     main()
