73a4118371d1fef976682f2697a9ae3e6c2dbd5e
Oso Olamide <olamideoso65@gmail.com>
2022-06-06 00:26:15 -0700

    script

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = len(my_list) - 0
-    for idx in range(0, my_list):
         if idx < 0 or idx > i:
             return None
         else:
