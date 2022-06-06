93a7888a6e00dff78dc8db024da9604bf05e638c
VicvekSr <Olamideoso65@gmail.com>
2022-06-06 08:11:13 +0000

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    a = len(my_-list) - 1
+    if idx >= 0 and idx <= a:
+        mylist[idx] = element
+        return my_list
+    else:
+        return my_list
+        
