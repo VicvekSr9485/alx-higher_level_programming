66a2717ae4ba872135d997a988a227bd616ed2bd
VicvekSr <Olamideoso65@gmail.com>
2022-06-20 14:18:11 +0000

    try/except

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     while True:
         try:
             if i < x:
-                print("{}".format(my_list[i]), end='')
+                print("{:d}".format(my_list[i]), end='')
                 i += 1
                 j += 1
             else:
