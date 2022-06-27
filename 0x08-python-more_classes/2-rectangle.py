6207b0a4cf3b82839d675e871d2897e5a2f535d5
VicvekSr <Olamideoso65@gmail.com>
2022-06-27 22:22:32 +0000

    rectangle class

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Defines a Rectangle class """
+
+
+class Rectangle:
+    """ This is a Rectangle class """
+
+    def __init__(self, width=0, height=0):
+        """ this initialize the instances """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """ this retrieves the width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ sets the width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """ this retrieves the height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ sets the height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """ This returns the rectangle area """
+        return(self.__height * self.__width)
+
+    def perimeter(self):
+        """ This returns the perimeter of the rectangle """
+        if (self.__height or self.__width) == 0:
+            return 0
+        else:
+            return(2 * (self.__height + self.__width))
