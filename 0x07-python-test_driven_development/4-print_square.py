a02d5783f4fafb8db0ee9098ecf7b0680f9d16a9
VicvekSr <Olamideoso65@gmail.com>
2022-06-27 08:48:09 +0000

    prints square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Square-printing function """
+
+
+def print_square(size):
+    """ function that prints a square using '#' character
+
+    args:
+        size: an integer that that represents the height and width if the square
+
+    raises:
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
