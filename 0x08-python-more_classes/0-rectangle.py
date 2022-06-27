5a10523c865a945356cdab6633cc137a57f57f1b
VicvekSr <Olamideoso65@gmail.com>
2022-06-27 20:37:36 +0000

    empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+This Module defines an empty Rectangle Class
+"""
+
+
+class Rectangle:
+    """ an empty rectangle object """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This is ALX High Level Programming Language Project:- More Python Classes and Object
