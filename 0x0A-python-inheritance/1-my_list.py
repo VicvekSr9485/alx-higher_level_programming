2b1533332f9e0154e7cbb337e470898702947b35
VicvekSr <Olamideoso65@gmail.com>
2022-07-04 11:33:03 +0000

    inherits a list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class inheritance 
+"""
+
+
+class MyList(list):
+    """ a subclass of integer lists
+    """
+
+    def print_sorted(self):
+        """ function that prints a sorted list of integers
+        """
+        print(sorted(self))
