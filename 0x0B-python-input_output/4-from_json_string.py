30dba3c8992b613b2f6e531d22aebf023ddcdfb5
VicvekSr <Olamideoso65@gmail.com>
2022-07-05 20:13:32 +0000

    json to object representation

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pythpn3
+""" json to object representation
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """ This function returns the object representation of a json file
+    """
+    return json.loads(my_str)
