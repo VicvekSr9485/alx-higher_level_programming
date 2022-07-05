a4c3bac9885e584e31f9ae8be6031ad57dba75c0
VicvekSr <Olamideoso65@gmail.com>
2022-07-05 18:48:11 +0000

    read text file to the stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Reading a text file to the stdout
+"""
+
+
+def read_file(filename=""):
+    """ This function reads a text file (UTF8) and 
+    prints it to stdout
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        read_text = f.read()
+        print(read_text, end="")
