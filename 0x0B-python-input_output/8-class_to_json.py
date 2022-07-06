683e60bb779cb52538648ebb1afe3fddfc098100
VicvekSr <Olamideoso65@gmail.com>
2022-07-06 08:59:19 +0000

    dictionary description with data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Class to json module
+"""
+
+
+def class_to_json(obj):
+    """ function that returns the dictionary description
+    with simple data structure
+    """
+    return obj.__dict__
