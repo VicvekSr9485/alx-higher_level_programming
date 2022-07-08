1b218ce71425b2425e663488d584aa45332d995c
VicvekSr <Olamideoso65@gmail.com>
2022-07-08 15:01:49 +0000

    almost a circle base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A Base class
+"""
+
+
+class Base:
+    """ The base class for other classes in this project
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ The init method for base class
+        """
+        if id is not None:
+            self.id = id
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects
