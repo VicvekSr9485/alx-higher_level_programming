78ad339dd4285bc87969a2a0e298d172ba617b4a
VicvekSr <Olamideoso65@gmail.com>
2022-07-09 23:03:13 +0000

    update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,23 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+
+    def args_update(self, id=None, size=None, x=None, y=None):
+        """ Updating the class Rectangle by assigning arguments
+        to each attributes """
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        """ Updates the both keyword and non-keyword arguments
+        """
+        if args:
+            self.args_update(*args)
+        else:
+            self.args_update(**kwargs)
