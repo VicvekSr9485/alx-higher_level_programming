5df4e715b42b7a10c8108436597371e7ca3c1f80
VicvekSr <Olamideoso65@gmail.com>
2022-07-09 22:44:36 +0000

    setter and getter methods of square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,4 +14,18 @@ class Square(Rectangle):
 
     def __str__(self):
         '''Returns string info about this square.'''
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}" \
+            .format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """ getter method for size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ setter method for size
+        """
+        self.width = value
+        self.height = value
