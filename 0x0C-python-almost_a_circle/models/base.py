db2567424a0f17ccd842d8adf104f2dca9e9a76e
VicvekSr <Olamideoso65@gmail.com>
2022-07-10 00:03:11 +0000

    dictionary to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ A Base class
 """
+import json
 
 
 class Base:
@@ -16,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ dictionary to json string
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
