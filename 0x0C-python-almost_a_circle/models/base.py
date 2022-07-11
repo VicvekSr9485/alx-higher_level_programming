49bf7bd7b49ec6443d536e8715f7dde64fab5343
VicvekSr <Olamideoso65@gmail.com>
2022-07-11 18:21:08 +0000

    Loads from json object

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ A Base class
 """
 import json
+import csv
 
 
 class Base:
@@ -71,3 +72,44 @@ class Base:
             with open(file_name, "r", encoding="utf-8") as f:
                 return [cls.create(**instances) for instances
                         in cls.from_json_string(f.read())]
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ Saves json object to csv file
+        """
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        file_name = "{}.csv".format(cls.__name__)
+        if list_objs is not None:
+            if cls is Rectangle:
+                 list_objs = [[o.id, o.width, o.height, o.x, o.y]
+                                for o in list_objs]
+            else:
+                list_objs = [[o.id, o.size, o.x, o.y]
+                                for o in list_objs]
+        with open(file_name, "w", newline="", encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerows(list_objs)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ Loads from json object
+        """
+        from models.rectangle import Rectangle
+        from models.square import Square
+        
+        ret = []
+        with open('{}.csv'.format(cls.__name__), "r", newline="",
+                  encoding="utf-8") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                row = [int(r) for r in row]
+                if cls is Rectangle:
+                    d = {"id": row[0], "width": row[1], "height": row[2],
+                         "x": row[3], "y": row[4]}
+                else:
+                    d = {"id": row[0], "size": row[1],
+                         "x": row[2], "y": row[3]}
+                ret.append(cls.create(**d))
+        return ret
