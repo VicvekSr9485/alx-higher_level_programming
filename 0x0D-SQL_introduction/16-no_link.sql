af1fb05321edd065658674fb4adf554d2f6dea0f
VicvekSr <Olamideoso65@gmail.com>
2022-08-09 13:39:06 +0000

    SELECT ALL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 --  script that lists all records of the table second_table
 -- of the database hbtn_0c_0 in MySQL server.
-SELECT score, name FROM second_table
+SELECT score, name FROM second_table WHERE name != NULL
 ORDER BY score DESC;
