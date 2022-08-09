9d2ac1c2f5c1f04d2f743831e9534bc51cf2cdda
VicvekSr <Olamideoso65@gmail.com>
2022-08-09 12:36:54 +0000

    add insert function

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,5 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in MySQL server
-INSERT IN first_table (id, name)
-VALUES (89, 'Best School');
+INSERT IN first_table (
+    id = 89
+    name = Best School
+);
