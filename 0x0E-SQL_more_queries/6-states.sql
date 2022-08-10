8b1aa9b795d2c78bb85585232cfa5ac56c34507c
VicvekSr <Olamideoso65@gmail.com>
2022-08-10 18:20:32 +0000

    create table with auto generated ids

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 --  This script creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
