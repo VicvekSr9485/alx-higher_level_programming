19e3d9041a9972f293e8505ed26fbc0db58dab92
VicvekSr <Olamideoso65@gmail.com>
2022-09-05 17:11:53 +0000

    add sprint square script

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else {
-  const size = parseInt(process.argv[2]);
+  let size = parseInt(process.argv[2]);
   for (let i = 0; i < size; i++) {
     let square = ' ';
     for (let j = 0; j < size; j++) {
