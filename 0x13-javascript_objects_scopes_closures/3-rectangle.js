341b6760f19159372475f74b31b3ec6c77ec2abc
VicvekSr <Olamideoso65@gmail.com>
2022-09-06 13:46:41 +0000

    add an instance method print

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,11 +9,10 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let rect = ' ';
       for (let j = 0; j < this.width; j++) {
-        rect += 'X';
+        process.stdout.write('X');
       }
-      console.log(rect);
+      console.log();
     }
   }
 };
