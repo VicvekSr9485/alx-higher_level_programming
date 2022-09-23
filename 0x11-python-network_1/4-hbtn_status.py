22e3ab847a9d0851c914935d38cb8db4cde146c1
VicvekSr <Olamideoso65@gmail.com>
2022-09-23 17:01:48 +0000

    What's my status task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,4 +8,5 @@ import requests
 if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')
     res = req.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(res), res))
+    print('Body response:')
+    print('\t- type: {}\n\t- content: {}'.format(type(res), res))
