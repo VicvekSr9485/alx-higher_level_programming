f8599a32f9b7fe3e0e1318893849efa3553d76de
VicvekSr <Olamideoso65@gmail.com>
2022-09-23 17:21:43 +0000

    add a script that sends a request to a url

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -12,6 +12,6 @@ import sys
 if __name__ == "__main__":
     res = requests.get(sys.argv[1])
     if res.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+        print("Error code: {}".format(res.status_code))
     else:
-        print(es.text)
+        print(res.text)
