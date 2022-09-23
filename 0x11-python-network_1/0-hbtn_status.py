be3a594a14af9730fb78f1bb83631eee07aab29a
VicvekSr <Olamideoso65@gmail.com>
2022-09-23 16:06:48 +0000

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         stat = response.read()
-        print('Body response:', end='$\n')
-        print('\t- type: {}'.format(type(stat)), end='$\n')
-        print('\t- content: {}'.format(stat), end='$\n')
-        print('\t- utf8 content: {}'.format(stat.decode("utf-8")), end='$\n')
+        print('Body response:')
+        print('\t- type: {}'.format(type(stat)))
+        print('\t- content: {}'.format(stat))
+        print('\t- utf8 content: {}'.format(stat.decode("utf-8")))
