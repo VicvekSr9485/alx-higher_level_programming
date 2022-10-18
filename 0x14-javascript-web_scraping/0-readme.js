41e1be8b16a16ed728e9159ba00cba7a29769eb9
VicvekSr <Olamideoso65@gmail.com>
2022-10-18 14:08:33 +0000

    read file using fs module

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const request = require('request');
-path = process.argv[2];
-fs.readFile(path, 'utf8', function(err, data) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(data);
-    }
+filePath = process.argv[2];
+fs.readFile(filePath, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
